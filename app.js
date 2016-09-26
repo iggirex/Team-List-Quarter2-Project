dcffe6ab0aebadd20f2f69e2ae0e657da063c5ce
Lane Milos <lanemilos@hotmail.com>
2016-09-26 14:03:10 -0600

    Removed features that are not active in order to activate express server

--- app.js
+++ app.js
@@ -25,8 +25,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 app.use('/', routes);
 app.use('/users', users);
