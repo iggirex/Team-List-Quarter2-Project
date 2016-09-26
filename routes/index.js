4c43895c7811637b1058d466c7d3b849bd48607f
Lane Milos <lanemilos@hotmail.com>
2016-09-26 10:41:09 -0600

    Required passport file into index

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+var query = require('./db/query')
+
+var passport = require('../passport')
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
