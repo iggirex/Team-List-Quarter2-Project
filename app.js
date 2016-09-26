5770542fa5200ca08f516948b4102d78659faaf6
Lane Milos <lanemilos@hotmail.com>
2016-09-26 10:41:32 -0600

    Added requirment from express-sessions to express-session

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var session = require('express-sessions')
+var session = require('express-session')
 var passport = require('./passport')
 
 var routes = require('./routes/index');
