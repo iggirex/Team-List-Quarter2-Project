b2470ef8fc6b3268df0db329e1b0ccc17118901c
Tyler Chase <tyler.chase.usa@gmail.com>
2016-09-27 15:48:45 -0600

    added auth

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,44 @@
 var express = require('express');
 var router = express.Router();
+var auth = require('../passport.js')
+var db = require('../db/query.js')
 
-var query = require('../db/query')
-
-var passport = require('../passport')
+//var query = require('../db/query')
+//var passport = require('../passport')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('pages/index', { title: 'Express' });
+  res.render('index', { title: 'Express' });
 });
+
+router.get('/profile', auth.ensureAuthenticated, function(req, res) {
+    res.json(req.body);
+});
+
+router.get('/login', function(req, res) {
+    res.render('login', {
+        user: req.user
+    });
+});
+
+router.get('/auth/google', auth.passport.authenticate('google', {
+    scope: [
+        'profile', 'email',
+    ],
+    accessType: 'offline',
+    approvalPrompt: 'force'
+}));
+
+router.get('/auth/google/callback',
+    auth.passport.authenticate('google', {
+        successRedirect: '/profile',
+        failureRedirect: '/login'
+      }
+    )
+);
 
 module.exports = router;
 
 router.get('/chat', function(req, res, next) {
-  res.render('pages/chat')
+  res.render('chat')
 })
