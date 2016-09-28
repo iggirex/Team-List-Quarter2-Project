6f2566e2a2c4d513ed6e44e357ed1c65d7b5cb68
Samantha Spensley <spenslsm@gmail.com>
2016-09-28 12:12:19 -0600

    add register query

--- db/query.js
+++ db/query.js
@@ -3,11 +3,21 @@ var knex = require('./knex.js');
 var Users = function(){
   return knex('users')
 }
+
 module.exports = {
   getAllUsers : function(){
     return Users();
   },
   getAllUsersByIdAndGoogleProfileId : function(profile){
     return Users().where('id', profile.id)
+  },
+  registerNewUser: function(id, user_name, genre, musical_instrument, influence, bio) {
+    return Users().where('id', id).update({
+      user_name: user_name,
+      genre: genre,
+      musical_instrument: musical_instrument,
+      influence: influence,
+      bio: bio
+    })
   }
 }
