fc4f4c497ad06d33260f2fb435a1f84841d7359e
Tyler Chase <tyler.chase.usa@gmail.com>
2016-09-28 15:12:58 -0600

    changed callback_url

--- passport.js
+++ passport.js
@@ -15,7 +15,7 @@ passport.deserializeUser(function(obj, done) {
 passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "http://localhost:3000/auth/google/callback", //"https://oauthtakethree.herokuapp.com/auth/google/callback",
+        callbackURL: process.env.GOOGLE_CALLBACK_URL, //"https://oauthtakethree.herokuapp.com/auth/google/callback",
         // "http://localhost:3000/auth/google/callback" ||
         passReqToCallback: true
     },
