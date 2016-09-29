00310ccf4bdd6e7383a101bf351a91164025604d
Samantha Spensley <spenslsm@gmail.com>
2016-09-28 19:45:01 -0600

    rewokring register routes

--- routes/index.js
+++ routes/index.js
@@ -28,11 +28,34 @@ router.get('/profile', auth.ensureAuthenticated,  function(req, res, next) {
 })
 
 router.get('/register', auth.ensureAuthenticated, function(req, res, next) {
-  res.render('register')
+  console.log('this is the req.user.id in /register :', req.user.id)
+  console.log('this is the req.user[0].id in /register :', req.user[0].id)
+
+  query.doesIDExist(req.user.id, req.user[0].id)
+  .then((user)=>{
+    if (!user.user_name){
+      console.log("inside ")
+      res.render('register')
+    } else {
+      console.log('user.user_name already exists')
+      res.redirect('/chat')
+    }
+  })
+  .catch((err)=>{
+    console.log(err)
+  })
+  //
+  // console.log("this is req.user: ", req.user)
+  // if (req.user.user_name === undefined) {
+  //   res.render('register')
+  // } else {
+  //   res.redirect('/chat')
+  // }
 });
 
 router.post('/register',   function(req, res, next) {
-  query.insertAdditionalInfo(req.user[0], req.body.user_name, req.body.genre, req.body.instrument, req.body.influence, req.body.bio)
+  console.log('this is req.user from the post:', req.user)
+  query.insertAdditionalInfo(req.user.id, req.body.user_name, req.body.genre, req.body.instrument, req.body.influence, req.body.bio)
   .then(() =>{
     res.redirect('/chat');
   })
