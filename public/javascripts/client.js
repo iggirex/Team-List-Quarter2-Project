3a93bf1c9a9501a36fe888b2f63c1d2e8765ca05
Iggi <iggirex@gmail.com>
2016-09-29 19:33:45 -0600

    attempting start and stop buttons

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -10,10 +10,12 @@ $(function(){
       $("#m").val("");
     })
 
-    $("#startVideo").click(function(e){
-      console.log("button just got clicked")
-      e.preventDefault();
-      socket.emit("start button");
+
+    $("#stopVideo").click(function(e){
+        console.log("button just got clicked")
+        e.preventDefault();
+        webrtc.pauseVideo();
+        // socket.emit("stop button");
     })
 
 
@@ -22,47 +24,40 @@ $(function(){
         $("#chatDiv").append($("<div><strong>"+msg+"</strong></p></div>"))
     })
 
-    // var webrtc = new SimpleWebRTC(options);
-// webrtc.on('startFeed', function (sessionId) {
-//   console.log("WE have received emission")
-//   var webrtc = new SimpleWebRTC({
-//       // the id/element dom element that will hold "our" video
-//       localVideoEl: 'localVideo',
-//       // the id/element dom element that will hold remote videos
-//       remoteVideosEl: 'remoteVideos',
-//       // immediately ask for camera access
-//       autoRequestMedia: true
-//   });
-// })
-
-socket.on("startFeed", function(msg){
-    console.log("Inside socke.on startFeed")
     var webrtc = new SimpleWebRTC({
-    // the id/element dom element that will hold "our" video
-    localVideoEl: 'localVideo',
-    // the id/element dom element that will hold remote videos
-    remoteVideosEl: 'remoteVideos',
-    // immediately ask for camera access
-    autoRequestMedia: true
-  });
-    webrtc.joinRoom('jamazon');
+      // the id/element dom element that will hold "our" video
+      localVideoEl: 'localVideo',
+      // the id/element dom element that will hold remote videos
+      remoteVideosEl: 'remotesVideos',
+      // immediately ask for camera access
+      autoRequestMedia: true
+      // socket.emit("creating initial")
+    });
 
-    $("#stopVideo").click(function(e){
-        console.log("button just got clicked")
-        e.preventDefault();
-        webrtc.pauseVideo();
-        // socket.emit("stop button");
+    webrtc.on('readyToCall', function () {
+  // you can name it anything
+  webrtc.joinRoom('your awesome room name');
+});
+
+    $("#startVideo").click(function(e){
+      console.log("button just got clicked")
+      e.preventDefault();
+      // socket.emit("start button");
+      webrtc.resumeVideo();
     })
-})
 
-    // webrtc.on('startFeed', function () {
-    //     // you can name it anything
-    //     webrtc.joinRoom('your awesome room name');
-    // });
+// socket.on("startFeed", function(msg){
+//     console.log("Inside socke.on startFeed")
+//     var webrtc = new SimpleWebRTC({
+//     // the id/element dom element that will hold "our" video
+//     localVideoEl: 'localVideo',
+//     // the id/element dom element that will hold remote videos
+//     remoteVideosEl: 'remoteVideos',
+//     // immediately ask for camera access
+//     autoRequestMedia: true
+//   });
+//     webrtc.joinRoom('jamazon');
 
-    // we have to wait until it's ready
-    // webrtc.on('readyToCall', function () {
-    //     // you can name it anything
-    //     webrtc.joinRoom('your awesome room name');
-    // });
+//
+// })
 })
